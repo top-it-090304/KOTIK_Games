4d81546bb6b46d276712d740f4696e740565a26e
vlad670 <vladchernobrivenko@gmail.com>
2026-03-13 19:06:37 +0600

    доработка сцены с карточками

--- новый-игровой-проект/sciens/windows_UI/Распределение ролей/Определение_роли.gd
+++ новый-игровой-проект/sciens/windows_UI/Распределение ролей/Определение_роли.gd
@@ -1,17 +1,13 @@
 extends Panel
 
-@onready var screen_size = DisplayServer.screen_get_size(DisplayServer.window_get_current_screen())
 @onready var card_animation = $MarginContainer/VBoxContainer/AnimationPlayer
 @onready var button = $MarginContainer/VBoxContainer/Button2
 @onready var lineedit = $MarginContainer/VBoxContainer/LineEdit
 @onready var audio = $AudioStreamPlayer
-
+var turned_over = false
 var name_
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print(screen_size)
-	get_window().size = screen_size
-	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,11 +17,20 @@ func _ready() -> void:
 
 
 func _on_button_2_pressed() -> void:
-	#audio.playing = true 
-	card_animation.play("RESET")
-	button.disabled = true
+	if turned_over == false:
+		card_animation.play("forward")
+		audio.playing = true
+		turned_over = true
+	elif turned_over == true:
+		card_animation.play_backwards("back")
+		audio.playing = true
+		turned_over = false
+	
 
 
-func _on_line_edit_editing_toggled(toggled_on: bool) -> void:
+func _on_line_edit_editing_toggled(_toggled_on: bool) -> void:
 	name_ = lineedit.text
 	print(name_)
+	
+func _on_button_pressed() -> void:
+	pass
