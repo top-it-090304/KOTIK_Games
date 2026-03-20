72c3241e1f3235c4c4106c6f67e352c10c21830a
vlad670 <vladchernobrivenko@gmail.com>
2026-03-20 15:15:26 +0600

    переработка главного меню

--- новый-игровой-проект/sciens/windows_UI/main/Play.gd
+++ новый-игровой-проект/sciens/windows_UI/main/Play.gd
@@ -1,1 +1,5 @@
 extends Control
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://sciens/windows_UI/main_screen/menu_container.tscn")
--- новый-игровой-проект/sciens/windows_UI/main_screen/main.gd
+++ новый-игровой-проект/sciens/windows_UI/main_screen/main.gd
@@ -1,47 +1,24 @@
 extends Control
 
-@onready var online = preload("res://sciens/game/game.tscn").instantiate()
-@onready var ofline = preload("res://sciens/windows_UI/main/Play.tscn").instantiate()
-@onready var play_panel = $PlayPanel
-@onready var settings_panel = $SettingsPanel
-@onready var exit_panel = $ExitPanel
-@onready var main_panel = $MainPanel
+@onready var ofline = preload("res://sciens/windows_UI/Распределение ролей/control_расп.tscn")
+@onready var play_panel = $"Главный_экран_1"
+@onready var settings_panel = $"Главный_экран_Настройки"
 
-func _ready():
-	# Скрываем все панели при запуске
-	hide_all_panels()
-	main_panel.show()
+
 
 func hide_all_panels():
 	play_panel.hide()
 	settings_panel.hide()
-	exit_panel.hide()
-	main_panel.hide()
 
-	
 
-func _on_settings_button_pressed():
+func _on_играть_pressed() -> void:
+	get_tree().change_scene_to_file("res://sciens/windows_UI/Распределение ролей/control_расп.tscn") 
+
+
+func _on_настройки_pressed() -> void:
 	hide_all_panels()
 	settings_panel.show()
 
-func _on_exit_button_pressed():
-	hide_all_panels()
-	exit_panel.show()
+
+func _on_выход_pressed() -> void:
 	get_tree().quit()
-
-func _on_back_button_pressed():
-	hide_all_panels()	
-	main_panel.show()
-
-
-func _on_plau_batton_pressed() -> void:
-	hide_all_panels()
-	play_panel.show()
-
-
-func _on_server_pressed() -> void:
-	get_tree().change_scene_to_file("res://sciens/game/game.tscn")
-
-
-func _on_client_pressed() -> void:
-	get_tree().change_scene_to_file("res://sciens/windows_UI/main/Play.tscn")
