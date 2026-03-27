0249d90f2d42f098e0cac7ca724e9940e40d9da3
vlad670 <vladchernobrivenko@gmail.com>
2026-03-27 17:00:01 +0600

    Изменение в дизайне

--- новый-игровой-проект/sciens/windows_UI/main_screen/main.gd
+++ новый-игровой-проект/sciens/windows_UI/main_screen/main.gd
@@ -1,25 +1,24 @@
 extends Control
 
-@onready var ofline = preload("res://sciens/windows_UI/Распределение ролей/control_расп.tscn")
+
 @onready var play_panel = $"Главный_экран_1"
 @onready var settings_panel = $"Главный_экран_Настройки"
 
-#func _ready() -> void:
-	#get_tree().ScreenOrientation = 1
+
 
 func hide_all_panels():
 	play_panel.hide()
 	settings_panel.hide()
 
 
-func _on_играть_pressed() -> void:
+func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://sciens/windows_UI/Распределение ролей/control_расп.tscn") 
 
 
-func _on_настройки_pressed() -> void:
+func _on_options_pressed() -> void:
 	hide_all_panels()
 	settings_panel.show()
 
 
-func _on_выход_pressed() -> void:
+func _on_quit_pressed() -> void:
 	get_tree().quit()
