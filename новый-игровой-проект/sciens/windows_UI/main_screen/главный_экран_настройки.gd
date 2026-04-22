bd6b2c9952e290fd8acc822a9e83e390f8e2e1ef
vlad670 <vladchernobrivenko@gmail.com>
2026-04-22 21:48:49 +0600

    Супер мега крутой экран опций был добавлен

--- новый-игровой-проект/sciens/windows_UI/main_screen/главный_экран_настройки.gd
+++ новый-игровой-проект/sciens/windows_UI/main_screen/главный_экран_настройки.gd
@@ -1,5 +1,7 @@
 extends Panel
 
+@onready var menu_bar = $MarginContainer/VBoxContainer/TabContainer/VBoxContainer2/HBoxContainer/MenuButton
+
 func _on_music_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(2,value)
 
@@ -8,3 +10,8 @@ func _on_effects_volum_value_changed(value: float) -> void:
 
 func _on_dictor_volum_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(3,value)
+
+
+func _on_menu_button_about_to_popup() -> void:
+	if menu_bar.text == "keep":
+		print("keep")
